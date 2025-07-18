f5685cefe0093fc96d0f7c3760a5dd0ac2cf7592
Kian Janloo <ddrdslayer@gmail.com>
2025-07-18 19:54:38 +0330

    Skills

--- src/utils/service/skills/skills.ts
+++ src/utils/service/skills/skills.ts
@@ -7,6 +7,7 @@ export const skills = [
     { name: 'Tailwind', rate: 100 },
     { name: 'ShadcnUi', rate: 100 },
     { name: 'Express', rate: 80 },
+    { name: 'NestJs', rate: 80 },
     { name: 'Git', rate: 100 },
     { name: 'react-hook-form', rate: 100 },
     { name: 'Formik', rate: 100 },    
