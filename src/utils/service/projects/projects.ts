75e88fd36207f1123d927745f3138ea090bf5c50
Kian Janloo <ddrdslayer@gmail.com>
2025-07-19 16:45:33 +0330

    Projects

--- src/utils/service/projects/projects.ts
+++ src/utils/service/projects/projects.ts
@@ -18,11 +18,11 @@ export const projects = [
         github: "https://github.com/KianJanloo/Game-Store",
         type: "Express",
     },
-    {
-        title: "Book City ( Back End )",
-        date: "02/2025",
-        link: "https://game-store-kappa-woad.vercel.app/",
-        github: "https://github.com/KianJanloo/Book-City",
-        type: "NestJs",
-    },
+    // {
+    //     title: "Book City ( Back End )",
+    //     date: "02/2025",
+    //     link: "https://game-store-kappa-woad.vercel.app/",
+    //     github: "https://github.com/KianJanloo/Book-City",
+    //     type: "NestJs",
+    // },
 ]
